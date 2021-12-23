a3338e9424ffcc539c84bcc7650c25fdb1f7413d
Elric1997 <thilobarth@hotmail.de>
2021-12-23 18:35:37 +0100

    Initial commit

---
+++ client.js
@@ -0,0 +1,10 @@
+// include the ipc module to communicate with main process.
+const ipcRenderer = require('electron').ipcRenderer;
+
+const btnclick = document.getElementById('loadnewwindow');
+btnclick.addEventListener('click', function () {
+    var arg ="secondparam";
+
+    //send the info to main process . we can pass any arguments as second param.
+    ipcRenderer.send("btnclick", arg); // ipcRender.send will pass the information to main process
+});
---
+++ index.js
@@ -0,0 +1,76 @@
+const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
+const fs = require('fs');
+const { app, BrowserWindow } = require('electron');
+const {ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
+
+function sniffDirec(){
+    let files = fs.readdirSync(__dirname + '/in')
+    files.forEach(function (file) {
+        fs.readFile(__dirname + '/in/' + file, (err,data) => {
+            if(err) throw err;
+            createPdf(data, file)
+        })
+
+        //console.log(doc); 
+        //createPdf(doc)
+    });
+}
+
+async function createPdf(document, name) {
+    //console.log(document);
+    const existingPdfBytes = document;
+    const pdfDoc = await PDFDocument.load(existingPdfBytes, {ignoreEncryption: true})
+    //const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+    //const page = pdfDoc.addPage()
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    const { width, height } = firstPage.getSize()
+    const fontSize = 8
+    firstPage.drawRectangle({
+        x: 5,
+        y: height - 2.5 * fontSize,
+        width: 85,
+        height: 15,
+        borderWidth: 1,
+        borderColor: rgb(1,0,0),
+        color: rgb(1,1,1),
+    });
+
+    firstPage.drawText('c/o ECHT-ID 157002', {
+        x: 10,
+        y: height - 2 * fontSize,
+        size: fontSize,
+        //font: timesRomanFont,
+        color: rgb(0, 0, 0),
+    })
+
+    fs.writeFileSync('./out/' + name , await pdfDoc.save());
+}
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.webContents.openDevTools();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+ipcMain.on("sign",function (event, arg) {
+    sniffDirec();
+});
+
